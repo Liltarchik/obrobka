5c1bdb353231aafdb97c0475d27ca48f13b326d3
Liltarchik <tarasikbilukha@gmail.com>
2026-05-13 17:59:59 +0300

    feat:lab10

---
+++ lab10/backend/src/main/java/com/example/courses_catalog/CoursesCatalogApplication.java
@@ -0,0 +1,11 @@
+package com.example.courses_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesCatalogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesCatalogApplication.class, args);
+    }
+}
---
+++ lab10/backend/src/main/java/com/example/courses_catalog/model/Course.java
@@ -0,0 +1,17 @@
+package com.example.courses_catalog.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "courses")
+@Data
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    
+    private String description;
+}
